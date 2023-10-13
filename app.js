ccb1849ef6e0e2581222a5909eb114a1a88c8886
Yalda Afshar <adlayafshar@gmail.com>
2023-10-13 16:42:26 +0100

    plan API endpoints

--- app.js
+++ app.js
@@ -1,12 +1,120 @@
 const express = require("express");
-const { getUsers } = require("./db/index");
+const { getUsers, createUsers, editUsers, deleteUsers } = require("./db/index");
 const app = express();
 const port = 3000;
 
-app.get("/", async (req, res) => {
-  const users = await getUsers();
-  res.send(JSON.stringify(users));
+// app.use(express.json());
+app.use(express.json());
+
+// app.get("/", async (req, res) => {
+//   const users = await getUsers();
+//   res.send(JSON.stringify(users));
+// });
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+    // const client = await pool.connect();
+    // const result = await client.query('SELECT * FROM users');
+    // const users = result.rows;
+    // client.release();
+    // res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
+app.post("/users", async (req, res) => {
+  const { first_name, last_name, password, email, address, account_name } =
+    req.body;
+  try {
+    const users = await createUsers(
+      first_name,
+      last_name,
+      password,
+      email,
+      address,
+      account_name
+    );
+    res.status(200).json(users);
+    // const client = await pool.connect();
+    // const result = await client.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
+    // const newUser = result.rows[0];
+    // client.release();
+    // res.status(201).json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// app.put("/", async (req, res) => {
+//   const users = await getUsers();
+//   res.send(JSON.stringify(users));
+// });
+
+// app.post("/", async (req, res) => {
+//   const users = await createUsers();
+//   res.send(JSON.stringify(users));
+// });
+
+// app.put('/updateUser/:id', async (req, res) => {
+//   const userId = req.params.id;
+//   const { name, email } = req.body;
+//   try {
+//       const client = await pool.connect();
+//       const result = await client.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *', [name, email, userId]);
+//       const updatedUser = result.rows[0];
+//       client.release();
+//       res.json(updatedUser);
+//   } catch (err) {
+//       console.error(err);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+app.put("/editUsers/:id", async (req, res) => {
+  const userId = parseInt(req.params.id);
+  const { first_name, last_name, password, address, account_name } = req.body;
+  try {
+    const users = await editUsers(
+      first_name,
+      last_name,
+      password,
+      address,
+      account_name,
+      userId
+    );
+    res.send(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+app.delete('/deleteUser/:id', async (req, res) => {
+  // const userId = req.params.id;
+  const userId = parseInt(req.params.id);
+  try {
+      // const client = await pool.connect();
+      // await client.query('DELETE FROM users WHERE id = $1', [userId]);
+      // client.release();
+      const users = await deleteUsers( userId );
+      // res.send(users);
+      res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// app.delete("/", async (req, res) => {
+//   const users = await getUsers();
+//   res.send(JSON.stringify(users));
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- db/index.js
+++ db/index.js
@@ -1,35 +1,79 @@
 // import { Pool} from "pg";
-
 const { Pool } = require("pg");
 
-process.env.PGUSER = "postgres";
-process.env.PGHOST = "localhost";
-process.env.PGPASSWORD = "postgres";
-process.env.PGDATABASE = "ecommerce";
-process.env.PGPORT = 5432;
+// const pool = new Pool();
+// process.env.PGUSER = "postgres";
+// process.env.PGHOST = "localhost";
+// process.env.PGPASSWORD = "postgres";
+// process.env.PGDATABASE = "ecommerce";
+// process.env.PGPORT = 5432;
 
-// const pool = new Pool({
-//     host: signerOptions.hostname,
-//     port: signerOptions.port,
-//     user: signerOptions.username,
-//     database: 'my-db',
-//     password: getPassword,
-//   })
-
-const pool = new Pool();
-
-// const res = await pool.query("SELECT NOW()");
-// await pool.end();
-
-// export const query = (text, params, callback) => {
-//   return pool.query(text, params, callback);
-// };
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "ecommerce",
+    password: "postgres",
+    port: 5432,
+});
 
 async function getUsers() {
-  const result = await pool.query("select * from users;");
-  console.log(result);
+    const result = await pool.query("select * from users;");
+    console.log(result);
+    return result.rows;
 
-  return result.rows;
+    // const client = await pool.connect();
+    // const result = await client.query('SELECT * FROM users');
+    // const users = result.rows;
+    // client.release();
+    // res.json(users);
 }
 
-module.exports = { getUsers };
+async function createUsers(
+    first_name,
+    last_name,
+    password,
+    email,
+    address,
+    account_name
+) {
+    const client = await pool.connect();
+    const result = await client.query(
+        `INSERT INTO users (first_name, last_name, password, email, address, account_name)
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+        [first_name, last_name, password, email, address, account_name]
+    );
+    const newUser = result.rows[0];
+    return newUser;
+}
+
+async function editUsers(
+    first_name,
+    last_name,
+    password,
+    address,
+    account_name,
+    userId
+) {
+    const client = await pool.connect();
+    const result = await client.query(
+        "UPDATE users SET first_name = $1, last_name = $2, password = $3, address = $4, account_name = $5 WHERE id = $6 RETURNING *",
+        [first_name, last_name, password, address, account_name, userId]
+    );
+
+    if (result.rowCount === 0) {
+        throw new Error(`User with ID ${userId} does not exist!`);
+    }
+
+    const updatedUser = result.rows[0];
+    return updatedUser;
+}
+
+
+
+async function deleteUsers( userId ) {
+    const client = await pool.connect();
+    await client.query('DELETE FROM users WHERE id = $1', [userId]);
+    // res.json({ message: 'User deleted successfully' });
+}
+
+module.exports = { getUsers, deleteUsers, editUsers, deleteUsers };
